81be019572a44d9f7d01b55587ca4991fa350214
John Tucker <john@larkintuckerllc.com>
2017-03-23 12:56:56 -0400

    More.

--- inactive/partials/futusign-youtube-missing-futusign.php
+++ inactive/partials/futusign-youtube-missing-futusign.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Missing futusign plugin partial.
  *
@@ -13,22 +12,31 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-$is_installed = Futusign_Youtube::is_plugin_installed( 'futusign' );
-$target = false;
-$action = __('Install', 'futusign_youtube');
-if ( current_user_can( 'install_plugins' ) ) {
-	if ( $is_installed ) {
-		$action = __('Activate', 'futusign_youtube');
-		$url = wp_nonce_url( self_admin_url( 'plugins.php?action=activate&plugin=' . $is_installed . '&plugin_status=active' ), 'activate-plugin_' . $is_installed );
+/**
+ * Show missing futusign
+ *
+ * @since    0.3.0
+ */
+function futusign_youtube_missing_futusign() {
+	$is_installed = Futusign_Youtube::is_plugin_installed( 'futusign' );
+	$target = false;
+	$action = __('Install', 'futusign_youtube');
+	if ( current_user_can( 'install_plugins' ) ) {
+		if ( $is_installed ) {
+			$action = __('Activate', 'futusign_youtube');
+			$url = wp_nonce_url( self_admin_url( 'plugins.php?action=activate&plugin=' . $is_installed . '&plugin_status=active' ), 'activate-plugin_' . $is_installed );
+		} else {
+			$url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=futusign' ), 'install-plugin_futusign' );
+		}
 	} else {
-		$url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=futusign' ), 'install-plugin_futusign' );
+		$target = true;
+		$url = 'http://wordpress.org/plugins/futusign/';
 	}
-} else {
-	$target = true;
-	$url = 'http://wordpress.org/plugins/futusign/';
+	?>
+	<div class="notice error is-dismissible">
+		<p><strong>futusign YouTube</strong> <?php esc_html_e('depends on the last version of futusign to work!', 'futusign_youtube' ); ?></p>
+		<p><a href="<?php echo esc_url( $url ); ?>" class="button button-primary"<?php if ( $target ) : ?> target="_blank"<?php endif; ?>><?php echo esc_html( $action . ' futusign' ); ?></a></p>
+	</div>
+	<?php
 }
-?>
-<div class="notice error is-dismissible">
-	<p><strong>futusign YouTube</strong> <?php esc_html_e('depends on the last version of futusign to work!', 'futusign_youtube' ); ?></p>
-	<p><a href="<?php echo esc_url( $url ); ?>" class="button button-primary"<?php if ( $target ) : ?> target="_blank"<?php endif; ?>><?php echo esc_html( $action . ' futusign' ); ?></a></p>
-</div>
+futusign_youtube_missing_futusign();
